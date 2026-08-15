1481da5975b17ef6a84762da9d9b4144fdfe9989
changcanno1 <dont.cry.my.little.angel.91@gmail.com>
2026-08-15 23:53:07 +0700

    Update Yanhh3d_plugin.js

--- Yanhh3d_plugin.js
+++ Yanhh3d_plugin.js
@@ -1,5 +1,5 @@
 // https://bilutv.asia
-BASEURL = "https://yanhh3d.love";
+BASEURL = "yanhh3d.pw";
 
 function getManifest() {
     return JSON.stringify({
@@ -8,7 +8,7 @@ function getManifest() {
         "description": "Trang xem phim Hoạt Hình siêu hay.",
       	"info":"Trang này bị nhà mạng chặn nên cần dns để xem. Bạn tải app 1.1.1.1 về dùng hoặc thử bật DNS và DPI trong app này.",
         "version": "1.3.4",
-        "baseUrl": "https://yanhh3d.love",
+        "baseUrl": "yanhh3d.pw",
         "iconUrl": "https://bilutv.asia/img/bilutvlogo-ngang.jpg",
         "isEnabled": true,
         "layoutType": "HORIZONTAL",
