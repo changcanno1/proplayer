0eb69973e71c696ed48d2700a5a68be1c81fcc2d
changcanno1 <dont.cry.my.little.angel.91@gmail.com>
2026-09-13 07:23:41 +0700

    Update Animehay09New_plugin.js

--- Animehay09New_plugin.js
+++ Animehay09New_plugin.js
@@ -3,7 +3,7 @@
 // =============================================================================
 
 // CHỈ CẦN THAY ĐỔI TÊN MIỀN Ở ĐÂY NẾU WEB ĐỔI ĐỊA CHỈ MỚI
-var DOMAIN = "https://animehay11.site";
+var DOMAIN = "https://animehay12.site";
 
 function getManifest() {
     return JSON.stringify({
